1ac72ea03e6069c28b0d27d75157077939cb7244
nikelei <zlooo52033@163.com>
2017-09-07 23:53:04 +0800

    修改mongodb服务器地址

--- config/default.js
+++ config/default.js
@@ -5,5 +5,5 @@ module.exports = {
         key: 'zBlog',
         maxAge: 2592000000
     },
-    mongodb: 'mongodb://localhost:27017/zBlog'
+    mongodb: 'mongodb://zoulei:zoulei1463.%3f@cluster0-shard-00-00-tggul.mongodb.net:27017,cluster0-shard-00-01-tggul.mongodb.net:27017,cluster0-shard-00-02-tggul.mongodb.net:27017/zBlog?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 };
